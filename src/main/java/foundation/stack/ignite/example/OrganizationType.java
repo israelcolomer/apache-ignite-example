f9e3fbb26ae4b4375a76bea5ddd63a8618bb6155
Israel Colomer <israelcolomer@gmail.com>
2017-04-24 18:15:37 +0100

    Initial commit

---
+++ src/main/java/foundation/stack/ignite/example/OrganizationType.java
@@ -0,0 +1,15 @@
+package foundation.stack.ignite.example;
+
+/**
+ * Organization type enum.
+ */
+public enum OrganizationType {
+    /** Non-profit organization. */
+    NON_PROFIT,
+
+    /** Private organization. */
+    PRIVATE,
+
+    /** Government organization. */
+    GOVERNMENT
+}
